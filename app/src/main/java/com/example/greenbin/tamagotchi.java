ab7b3e8e25987e75b2bcec6c2c07800edb888c27
hackathon19 <hackathon19@zone38-wd.cs.ucalgary.ca>
2019-02-17 05:56:11 -0700

    return of the button

--- app/src/main/java/com/example/greenbin/tamagotchi.java
+++ app/src/main/java/com/example/greenbin/tamagotchi.java
@@ -2,12 +2,10 @@ package com.example.greenbin;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.constraint.solver.widgets.Rectangle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.ButtonBarLayout;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 
 public class tamagotchi extends AppCompatActivity {
@@ -20,23 +18,17 @@ public class tamagotchi extends AppCompatActivity {
         ImageView trash = findViewById(R.id.trash);
         trash.setVisibility(View.VISIBLE);
 
-
-//        final Button button = findViewById(R.id.ToMapButton);
-//        button.setOnClickListener(new View.OnClickListener() {
-//            public void onClick(View v) {
-//                // Code here executes on main thread after user presses button
-//                next_page(v);
-//            }
-//        });
-//
-//    }
-//
-//
-//    public void next_page(View v) {
-//        Intent intent = new Intent(this, MapsActivity.class);
-//        startActivity(intent);
+        final ButtonBarLayout button = findViewById(R.id.gps);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                next_page(v);
+            }
+        });
     }
 
-
-
+    public void next_page(View v) {
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+    }
 }
