02c310080210e0f7cd4f88e022a5dc19c9068e7e
ttoocs <ttoocs@gmail.com>
2019-02-17 05:03:19 -0700

    Made it scan stuffgit add -pgit add -p

--- app/src/main/java/com/example/greenbin/SimpleScannerActivity.java
+++ app/src/main/java/com/example/greenbin/SimpleScannerActivity.java
@@ -1,45 +1,69 @@
 package com.example.greenbin;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
 import com.google.zxing.Result;
-
-import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
 import static android.support.constraint.Constraints.TAG;
 
-public class SimpleScannerActivity extends Activity implements ZXingScannerView.ResultHandler {
-    private ZXingScannerView mScannerView;
+public class SimpleScannerActivity extends AppCompatActivity {
+    private CodeScanner mCodeScanner;
 
     @Override
-    public void onCreate(Bundle state) {
-        super.onCreate(state);
-        mScannerView = new ZXingScannerView(this);   // Programmatically initialize the scanner view
-        setContentView(mScannerView);                // Set the scanner view as the content view
+    protected void onCreate(Bundle savedInstanceState) {
+
+        //Get permissions
+
+        while (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) { ActivityCompat.requestPermissions(SimpleScannerActivity.this, new String[] {Manifest.permission.CAMERA}, 42); try { Thread.sleep(10); } catch(Exception e) {} }
+
+
+                super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scanner);
+        CodeScannerView scannerView = findViewById(R.id.scanner_view);
+        mCodeScanner = new CodeScanner(this, scannerView);
+        final Activity activity = SimpleScannerActivity.this;
+        mCodeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull final Result result) {
+                activity.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(activity, result.getText(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+        scannerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCodeScanner.startPreview();
+            }
+        });
     }
 
     @Override
-    public void onResume() {
+    protected void onResume() {
         super.onResume();
-        mScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.
-        mScannerView.startCamera();          // Start camera on resume
+        mCodeScanner.startPreview();
     }
 
     @Override
-    public void onPause() {
+    protected void onPause() {
+        mCodeScanner.releaseResources();
         super.onPause();
-        mScannerView.stopCamera();           // Stop camera on pause
-    }
-
-    @Override
-    public void handleResult(Result rawResult) {
-        // Do something with the result here
-        Log.v(TAG, rawResult.getText()); // Prints scan results
-        Log.v(TAG, rawResult.getBarcodeFormat().toString()); // Prints the scan format (qrcode, pdf417 etc.)
-
-        // If you would like to resume scanning, call this method below:
-        mScannerView.resumeCameraPreview(this);
     }
 }
